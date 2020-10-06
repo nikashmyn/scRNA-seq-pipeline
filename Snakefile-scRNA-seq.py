bb4f8e5388f2cde6bb0c0386a4f6b0404a566abf
nikashmyn <67345995+nikashmyn@users.noreply.github.com>
2020-10-06 11:29:39 -0400

    update 10/6
    fixed merger

--- Snakefile-scRNA-seq.py
+++ Snakefile-scRNA-seq.py
@@ -51,6 +51,8 @@ rule recal:
 #premapping and aligning step for STAR
 rule generate_genome_indexes:
     input:
+        in_fasta = config["reference_unzip"],
+        in_gtf = config["reference_gtf_unzip"]
     output:
         ref_dir = "/pellmanlab/stam_niko/STAR/genome/"
     params:
@@ -65,20 +67,33 @@ rule generate_genome_indexes:
 #mapping, aligning, and sorting step
 rule STAR_alignment:
     input:
+        R1 = "/pellmanlab/stam_niko/data/ERR523111/{samples}_1.fastq", #.gz use readcmd
+        R2 = "/pellmanlab/stam_niko/data/ERR523111/{samples}_2.fastq", #.gz
         ref_dir = "/pellmanlab/stam_niko/STAR/genome/"
     output:
         mock = "/pellmanlab/stam_niko/data/bam/ERR523111/.{samples}_mockfile.txt"
     params:
+        names = "/pellmanlab/stam_niko/data/bam/ERR523111/raw_aligned_bams/{samples}.",
         runMode = "alignReads",
+        outstyle = "BAM Unsorted", 
+        unmapped = "Within KeepPairs",
+        twopass = "Basic",
+        tags = "NH HI AS NM MD"
+        #readcmd = "zcat"
     shell:
         "STAR --genomeDir {input.ref_dir} --readFilesIn {input.R1} {input.R2} "
+        "--runMode {params.runMode} --twopassMode {params.twopass} --outFileNamePrefix {params.names} " #--readFilesCommand {params.readcmd} "
+        "--outSAMunmapped {params.unmapped} --outSAMtype {params.outstyle} --outSAMattributes {params.tags} " # For RSEM compatability #like --alignbytype endtoend 
+        "&& touch {output.mock} " #For snakemake step tracing
 
 #add read group information and calc tags
 rule AddOrReplaceRG:
     input:
         mock = "/pellmanlab/stam_niko/data/bam/ERR523111/.{samples}_mockfile.txt"
     output:
+        bam_out = "/pellmanlab/stam_niko/data/bam/ERR523111/raw_aligned_bams/{samples}.Aligned.RG.out.bam",
     params:
+        bam_in = "/pellmanlab/stam_niko/data/bam/ERR523111/raw_aligned_bams/{samples}.Aligned.out.bam",
         rginfo = "--SORT_ORDER queryname --RGID {samples} --RGLB {samples} --RGPL illumina --RGPU {samples} --RGSM {samples} " #Needs to be revisited on real data
     shell:
         "picard AddOrReplaceReadGroups -I {params.bam_in} -O {output.bam_out} {params.rginfo} "
@@ -86,8 +101,12 @@ rule AddOrReplaceRG:
 #remove duplicates step        
 rule mark_duplicates:
     input:
+        bam_in = "/pellmanlab/stam_niko/data/bam/ERR523111/raw_aligned_bams/{samples}.Aligned.RG.out.bam"
     output:
+        metrics = "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.metrics.txt", #File to write duplication metrics.
+        bam_out = "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.deduped.out.bam"
     params:
+        options = f"--CREATE_INDEX true --REMOVE_DUPLICATES true --ASSUME_SORT_ORDER queryname --TMP_DIR {config['tmp_dir']} " 
     shell:
         "picard MarkDuplicates --INPUT {input.bam_in} {params.options} --OUTPUT {output.bam_out} --METRICS_FILE {output.metrics} "
        
@@ -96,7 +115,9 @@ rule sort_sam:
     input:
         bam_in = "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.deduped.out.bam"
     output:
+        bam_out =  "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.sorted.deduped.out.bam" 
     params:
+        SO = "coordinate", #Downstream tools need coord sorted
         options = "--CREATE_INDEX true"
     shell:
         "picard SortSam --INPUT {input.bam_in} {params.options} --OUTPUT {output.bam_out} --SORT_ORDER {params.SO} " 
@@ -104,7 +125,9 @@ rule sort_sam:
 #calc tags
 rule calctags:
     input:
+        bam_in = "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.sorted.deduped.out.bam"
     output:
+        bam_out = "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.sorted.deduped.tagged.out.bam"
     params:
         ref = config["reference_unzip"]
     shell:
@@ -113,7 +136,10 @@ rule calctags:
 # Split Reads with N in Cigar
 rule splitncigarreads:
     input:
+        bam_in =  "/pellmanlab/stam_niko/data/bam/ERR523111/mark_dups/{samples}.Aligned.sorted.deduped.tagged.out.bam", 
+        ref = config["ref_unzip_wdict"] 
     output:
+        bam_out = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.Aligned.sorted.deduped.split_r.out.bam"
     params:
         "--create-output-bam-index true"
         #f"--tmp-dir {config['tmp_dir']} "
@@ -123,12 +149,39 @@ rule splitncigarreads:
 # Base Quality Score Recalibration (BQSR) -- Generates recalibration table based on various user-specified covariates (such as read group, reported quality score, machine cycle, and nucleotide context)
 rule base_recal:
     input:
+        bam_in = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.Aligned.sorted.deduped.split_r.out.bam", 
+        ref = config["ref_unzip_wdict"], 
+        KS = config["ref_var"] #Known polymorphic sites used to exclude regions around known polymorphisms from analysis.
     output:
+<<<<<<< HEAD
         recal_out = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.recal_table.txt",  #The output recalibration table file to create  Required.
         bam_out  = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.Aligned.sorted.deduped.split_r.recal.out.bam",
+=======
+        recal_out = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.recal_table.txt",  #The output recalibration table file to create.
+        bam_out  = "/pellmanlab/stam_niko/data/bam/ERR523111/split_reads/{samples}.Aligned.sorted.deduped.split_r.recal.out.bam", #The BAM to build from out table ^
+>>>>>>> b5356e03f9f06ad8f29bbf03423f9004bcd54b67
     params:
         recal = "--read-filter PairedReadFilter",
         applybqsr = "--create-output-bam-index True"
     shell:
         "gatk BaseRecalibrator --input {input.bam_in} --reference {input.ref} --known-sites {input.KS} {params.recal} --output {output.recal_out} "
         "&& gatk ApplyBQSR --input {input.bam_in} --bqsr-recal-file {output.recal_out} --reference {input.ref} {params.applybqsr} --output {output.bam_out} "
+<<<<<<< HEAD
+=======
+
+#BAM input to RSEM differential expression 
+### TO DO ### Add BAM input options
+#rule rsem_calc_expr:
+#    input:
+#        R1 = f"/pellmanlab/stam_niko/data/ERR523111/{samples}_1.fastq",
+#        R2 = f"/pellmanlab/stam_niko/data/ERR523111/{samples}_2.fastq"
+#    params:
+#        ref_name = "EnsembleGenome98",
+#        sample_name = f"{samples}",
+#        options = "--star --calc-pme --calc-ci --output-genome-bam ",
+#        num_thrd = "--num-threads 4",
+#        seed = "--seed 12 "
+#    shell:
+#        "rsem-calculate-expression {params.options} {params.num_thrd} {params.seed} "
+#        "--paired-end {input.R1} {input.R2} {params.ref_name} {params.sample_name} "
+>>>>>>> b5356e03f9f06ad8f29bbf03423f9004bcd54b67
